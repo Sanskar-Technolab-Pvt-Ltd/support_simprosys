dc692f40398912aa2c72c17f421c8b039f8f4752
NilPatel1204 <nil@sanskartechnolab.com>
2025-07-16 15:58:31 +0530

    Reply to email set

--- support_simprosys/support_simprosys/api.py
+++ support_simprosys/support_simprosys/api.py
@@ -1361,14 +1361,11 @@ def send_support_ticket_email(docname):
 
     recipients_email = None
     if doc.plugin_or_app_related_queries == "Career opportunities":
-        # recipients_email = ["careers@simprosys.com"]
-        recipients_email = ["nil@sanskartechnolab.com"]
+        recipients_email = ["careers@simprosys.com"]
     elif doc.plugin_or_app_related_queries == "Partnership opportunities":
-        recipients_email = ["nil@sanskartechnolab.com"]
-        # recipients_email = ["partnerships@simprosys.com"]
+        recipients_email = ["partnerships@simprosys.com"]
     else:
-        recipients_email = ["nil@sanskartechnolab.com"]
-        # recipients_email = ["support@simprosys.com"]
+        recipients_email = ["support@simprosys.com"]
 
     if recipients_email:
         frappe.sendmail(
